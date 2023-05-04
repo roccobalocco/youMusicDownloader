77aa59db8914455b83bbe2f4bfddf4d63fe5978b
roccobalocco <pietro.masolini@studenti.unimi.it>
2023-05-04 18:53:55 +0200

    Playlists support and new elements in the GUI

--- utils/ytFuns.py
+++ utils/ytFuns.py
@@ -1,8 +1,8 @@
-from pytube import YouTube
+from pytube import YouTube, Playlist
 import os
 from pathlib import Path
 
-def youtube2mpX (outdir, url="https://youtu.be/xvFZjo5PgG0", mpx="mp3"):
+def youtube2mpX (outdir: str, url: str ="https://youtu.be/xvFZjo5PgG0", mpx: str="mp3")-> str:
     yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
     # Extract audio with the maximum quality from video
     video = yt.streams.get_audio_only()
@@ -12,8 +12,12 @@ def youtube2mpX (outdir, url="https://youtu.be/xvFZjo5PgG0", mpx="mp3"):
     out_file = video.download(output_path=outdir)
     base, ext = os.path.splitext(out_file)
     new_file = Path(f'{base}.{mpx}')
-    os.rename(out_file, new_file)
     
+    try:
+        os.rename(out_file, new_file)
+    except FileNotFoundError as _:
+        print('Rename is failed due to the presence of duplicate song url, don\'t worry the song will be in your directory')
+        
     # Check success of download
     if new_file.exists():
         print(f'{yt.title} has been successfully downloaded.')
@@ -21,3 +25,11 @@ def youtube2mpX (outdir, url="https://youtu.be/xvFZjo5PgG0", mpx="mp3"):
     else:
         print(f'ERROR: {yt.title}could not be downloaded!')
         return "an Error"
+    
+def youtubePlaylistExtractor(url: str)-> list[str]:
+    if not url.__contains__('&list'):
+        print("Song instead of playlist")
+        return []
+    playlist =  Playlist(url)
+    return playlist.video_urls
+    
